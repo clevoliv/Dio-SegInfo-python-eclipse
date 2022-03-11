8074d6513bd6ab9bcc5caab0468e3b3293e57bfb
cleversonoliveira <cleversonoliveira@gmail.com>
2022-03-11 12:37:01 -0300

    alterado de 6 para 4 repetições

--- pingsimples1/pingsimples/pingsimples.py
+++ pingsimples1/pingsimples/pingsimples.py
@@ -2,4 +2,7 @@ import os
 
 ip_ou_host = input("Digite o ip ou host a ser verificado: ")
 
-os.system(('ping -n 6 {}'.format(ip_ou_host)))
+os.system(('ping -n 4 {}'.format(ip_ou_host)))
+
+print("ping testado")
+
